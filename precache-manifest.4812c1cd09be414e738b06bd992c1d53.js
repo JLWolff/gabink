05ac778e26959c21e3371e854027e01c88e8cb66
JLWolff <joaolwolff@gmail.com>
2020-07-22 15:11:52 +0100

    Updates

---
+++ precache-manifest.4812c1cd09be414e738b06bd992c1d53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45a6949defbcdc4e7b473e3c05bb942",
+    "url": "/gabink/index.html"
+  },
+  {
+    "revision": "601faf386ca0f9c7fc2c",
+    "url": "/gabink/static/css/main.7068f1f6.chunk.css"
+  },
+  {
+    "revision": "7da16041304031c7b26d",
+    "url": "/gabink/static/js/2.6f827f99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gabink/static/js/2.6f827f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601faf386ca0f9c7fc2c",
+    "url": "/gabink/static/js/main.a04c58c3.chunk.js"
+  },
+  {
+    "revision": "9117cce4bfe4fdeacf85",
+    "url": "/gabink/static/js/runtime-main.acfe9da7.js"
+  },
+  {
+    "revision": "b44ac4848fa89e520d78790c784a029c",
+    "url": "/gabink/static/media/davi.b44ac484.jpg"
+  },
+  {
+    "revision": "4bc09e554838d791fc156e7ba18eab88",
+    "url": "/gabink/static/media/flor.4bc09e55.jpg"
+  }
+]);
